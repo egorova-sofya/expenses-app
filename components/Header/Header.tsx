e19ab6072f5c58ace34756cb1878fb36be6fb0d8
egorova-sofya <EgorovaSjob@yandex.ru>
2024-03-05 15:10:33 +0300

    added mock function for expense creating

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { FC } from "react";
 import { Image, View } from "react-native";
 import styles from "./header.style";
 import Notifications from "./../../assets/images/icons/notifications.svg";
 import Plus from "./../../assets/images/icons/plus.svg";
 import IconButton from "../Button/IconButton";
 import { COLORS } from "../../constants/theme";
+import { useDispatch } from "react-redux";
+import { addExpense } from "../../app/store/expensesSlice";
 
-const Header = () => {
+interface Props {}
+
+const Header: FC<Props> = ({}) => {
+  const dispatch = useDispatch();
+
+  const createExpense = () => {
+    dispatch(
+      addExpense({
+        title: "New Expense",
+        date: '2022-01-01"',
+        id: Math.floor(Math.random() * 1000),
+        price: 1000,
+      })
+    );
+  };
   return (
     <View style={styles.container}>
       <Image
@@ -17,7 +33,7 @@ const Header = () => {
         <IconButton>
           <Notifications width={22} height={22} fill={COLORS.white} />
         </IconButton>
-        <IconButton>
+        <IconButton onPress={createExpense}>
           <Plus width={22} height={22} fill={COLORS.white} />
         </IconButton>
       </View>
